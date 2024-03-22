e9463d5fe022a92a021eee6ff4cbcd2b4cd2269b
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-22 14:44:30 -0300

    Exercise solution.

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,12 +1,27 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
+    public String describe(Character character) {
+        return String.format("You're a level %d %s with %d hit points.",
+                character.getLevel(), character.getCharacterClass(), character.getHitPoints());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe(Destination destination) {
+        return String.format("You've arrived at %s, which has %d inhabitants.",
+                destination.getName(), destination.getInhabitants());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a TravelMethod
+    public String describe(TravelMethod travelMethod) {
+        return switch (travelMethod) {
+            case WALKING -> "You're traveling to your destination by walking.";
+            case HORSEBACK -> "You're traveling to your destination on horseback.";
+        };
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
+    public String describe(Character character, Destination destination, TravelMethod travelMethod) {
+        return String.format("%s %s %s", describe(character), describe(travelMethod), describe(destination));
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character and Destination
+    public String describe(Character character, Destination destination) {
+        return describe(character, destination, TravelMethod.WALKING);
+    }
 }
